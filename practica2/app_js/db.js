b751fa771467f45403d9d12dbd6f8234a1013b67
Marvin <madacast2@gmail.com>
2026-09-19 01:32:04 -0600

    BDJSUpdate

--- practica2/app_js/db.js
+++ practica2/app_js/db.js
@@ -5,8 +5,7 @@ const pool = new Pool({
     port: 5432,
     database: "postgres",
     user: "marvin",
-    password: "Seminario12026",
-    ssl:{ca:fs.readFileSync("{ca-cert filename}")}
+    password: "Seminario12026"
 });
 
 module.exports = pool;
